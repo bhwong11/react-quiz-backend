b5d65f85312259bbff440900ba466f362a65a649
bhwong11 <user@email.com>
2021-10-08 17:26:22 -0400

    successfully added atlas

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-const connectionString = 
-    process.env.MONGODB_URI || "mongodb://localhost:27017/quizzes";
+//const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/quizzes";
+const connectionString = process.env.MONGODB_URI;
 const configOptions ={
     useNewUrlParser:true,
     useUnifiedTopology:true,
